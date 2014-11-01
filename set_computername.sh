4e52de0e76afcd2ecd6f70597ec024379446dff3
flexmonkeypatch <flexmonkeypatches@gmail.com>
2014-11-01 12:09:33 -0500

    Update set_computername.sh

--- set_computername.sh
+++ set_computername.sh
@@ -20,6 +20,7 @@ case $response in
       sudo scutil --set HostName $COMPUTER_NAME
       sudo scutil --set LocalHostName $COMPUTER_NAME
       sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string $COMPUTER_NAME
-      break;;
-#  *) break;;
+#      break;;
+#  *) break
+     ;;
 esac
